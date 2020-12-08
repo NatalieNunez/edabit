302f7ade6f47bad7cdb36fb05f4c719c844397a7
NatalieNunez <natalie.nunez17@yahoo.com>
2020-12-07 16:27:35 -0800

    Modified Profit

--- profit.js
+++ profit.js
@@ -1,3 +1,11 @@
 function profitableGamble(prob, prize, pay) {
 	return (prob*prize > pay ? true : false);
 }
+function profitableGamble(prob, prize, pay) {
+	if (prob*prize > pay) {
+			return true; 
+		} else {
+			return false;
+		}
+	}
+	// return prob * prize > pay ? true : false;
