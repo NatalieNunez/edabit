a724b244bc038e02a5537ac7559a15df1014ade7
NatalieNunez <natalie.nunez17@yahoo.com>
2020-12-10 22:27:19 -0800

    Complete Area of Rectangle

---
+++ arearectangle.js
@@ -0,0 +1,12 @@
+function area(h, w) {
+	if (h <= 0 || w <= 0) {
+		return -1;
+	} else {
+		return h*w;
+	}
+}
+// input two numbers output one number
+// if numbers are positive, solve equation
+// if one or more numbers are negative return -1
+// use || or 
+// if else statement
