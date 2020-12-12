5677f42804ddb132f5b3e8ac11de81e1e4338304
NatalieNunez <natalie.nunez17@yahoo.com>
2020-12-11 21:18:45 -0800

    Complete Index Array

---
+++ arrindex.js
@@ -0,0 +1,9 @@
+function valueAt(arr, i) {
+	return arr[Math.floor(i)];
+}
+// input array and number
+// output number within array 
+// Math.floor(i)
+
+// instructions: Given an index and an array, return the value of the array with the given index.
+// example: valueAt([1, 2, 3, 4, 5, 6], 10 / 2) ➞ 6
