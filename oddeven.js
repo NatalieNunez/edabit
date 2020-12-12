2cbd8fc3d3b4255078e4b965d39586ff6413ba52
NatalieNunez <natalie.nunez17@yahoo.com>
2020-12-12 12:35:09 -0800

    Complete Check Even or Odd

---
+++ oddeven.js
@@ -0,0 +1,9 @@
+function isEvenOrOdd(num) {
+	if (num % 2 === 0) {
+		return "even";
+	} else {
+		return "odd";
+	}
+}
+// Create a function that takes a number as an argument and returns 
+// "even" for even numbers and "odd" for odd numbers
