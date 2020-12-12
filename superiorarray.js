701d5fa68958ae26bd4df8da875c6af1fec39412
NatalieNunez <natalie.nunez17@yahoo.com>
2020-12-11 21:29:07 -0800

    Complete Comparing Array Values

---
+++ superiorarray.js
@@ -0,0 +1,8 @@
+function isFirstSuperior(arr1, arr2) {
+	return arr1 > arr2 ? true : false;
+}
+// instructions: Create a function that returns whether the first array is slightly superior to that of the second.
+// input: two arrays
+// ouput: boolean
+// if arrays are the same, return false
+// ternary operator maybe?
