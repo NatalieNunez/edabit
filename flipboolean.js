2d7e110b6af88901128b2a3a83e2e37e3bf5a053
NatalieNunez <natalie.nunez17@yahoo.com>
2020-12-15 14:54:23 -0800

    Mod Flip Boolean

--- flipboolean.js
+++ flipboolean.js
@@ -1,3 +1,7 @@
 function flipBool(b) {
 	return b === true || b === 1 ? 0 : 1;
 }
+/* function flipBool(b) {
+	return b ? 0 : 1; 
+ } 
+ since its a boolean it will already return a truthy value so theres no need to set equal to true or 1 */
