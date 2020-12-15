4c3fdb75f44610b058a894b68f489706304bf28a
NatalieNunez <natalie.nunez17@yahoo.com>
2020-12-15 15:13:48 -0800

    Converted Array to String

---
+++ arrtostr.js
@@ -0,0 +1,10 @@
+function arrayToString(arr) {
+  let str = '';
+    for (let i = 0; i < arr.length; i++) {
+      str = str + arr[i];
+    }
+    return str;
+  }
+  /* function arrayToString(arr) {
+    return arr.join('');
+  } */
