04cc08efbfd4c1abbbc542508a5ee8cdcbf0f309
NatalieNunez <natalie.nunez17@yahoo.com>
2020-12-16 22:13:53 -0800

    Modified Index Problem

---
+++ findindex.js
@@ -0,0 +1,12 @@
+function search(arr, item) {
+  for (let i = 0; i < arr.length; i++) {
+   return arr.indexOf(item);
+  } 
+   }
+   /* function search(arr, item) {
+	if (arr.includes(item)) {
+		return arr.indexOf(item);
+} else {
+	return -1;
+ }
+} */
