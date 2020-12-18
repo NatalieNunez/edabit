6a5de18c68577e368a5a8ed5024e0ea735503d1d
NatalieNunez <natalie.nunez17@yahoo.com>
2020-12-17 21:11:57 -0800

    Complete milisec problem

---
+++ miliseconds.js
@@ -0,0 +1,5 @@
+function past(h, m, s){
+  return ((h*3600000)+(m*60000)+(s*1000));
+}
+// Clock shows 'h' hours, 'm' minutes and 's' seconds after midnight.
+// Your task is to make 'Past' function which returns time converted to milliseconds.
