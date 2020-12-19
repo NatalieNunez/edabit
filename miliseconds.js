ad9a19a1cd0ee7deb081e9856deeaeee4332d039
NatalieNunez <natalie.nunez17@yahoo.com>
2020-12-18 23:55:30 -0800

    Modified

--- miliseconds.js
+++ miliseconds.js
@@ -1,5 +1,5 @@
 function past(h, m, s){
-  return ((h*3600000)+(m*60000)+(s*1000));
+  return (1000*(h*3600000)+(m*60000)+s);
 }
 // Clock shows 'h' hours, 'm' minutes and 's' seconds after midnight.
 // Your task is to make 'Past' function which returns time converted to milliseconds.
