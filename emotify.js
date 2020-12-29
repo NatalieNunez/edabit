2c6c73c294af5636baad18c08ebc9bd565f36d44
NatalieNunez <natalie.nunez17@yahoo.com>
2020-12-28 23:40:04 -0800

    Modified emotify with explanations

--- emotify.js
+++ emotify.js
@@ -9,3 +9,19 @@ Test.assertEquals(emotify("Make me smile"), "Make me :D")
 Test.assertEquals(emotify("Make me grin"), "Make me :)")
 Test.assertEquals(emotify("Make me sad"), "Make me :(")
 Test.assertEquals(emotify("Make me mad"), "Make me :P") */
+
+function emotify(str) {
+	let [first, mid, last] = str.split(" "); // splits the string at every space and returns as an array
+	
+	let emotify = {
+		"smile": ":D",
+		"grin": ":)",
+		"sad": ":(",
+		"mad": ":P"
+	}
+	
+	return `Make me ${emotify[last]}`;
+}
+/* The split() method divides a String into an ordered list of substrings, puts these 
+substrings into an array,and returns the array.  The division is done by searching 
+for a pattern; where the pattern is provided as the first parameter in the method's call. 
