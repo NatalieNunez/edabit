7dbd71bfb9a5c7ccbb55e6d4ceb31bd9596ed7d0
NatalieNunez <natalie.nunez17@yahoo.com>
2021-01-03 19:43:25 -0800

    refactored problem

--- emptystring.js
+++ emptystring.js
@@ -1,6 +1,9 @@
 function isEmpty(s) {
+	return s === "";
+} // refactored; no need to use ternary, booleans will already return true or false
+/* function isEmpty(s) {
 	return (s === "" ? true : false);
 }
-// function isEmpty(s) {
-//	return s.length === 0 ? true : false;
-// }
+ function isEmpty(s) {
+	return s.length === 0 ? true : false;
+ } */
