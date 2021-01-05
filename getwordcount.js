582a69688445f9b49ad2a864d2f24c02abd13f0e
NatalieNunez <natalie.nunez17@yahoo.com>
2021-01-04 23:59:45 -0800

    Refactor

--- getwordcount.js
+++ getwordcount.js
@@ -2,3 +2,12 @@ function getWordCount(sentence) {
   let count = sentence.split();
   return count.length;
 }
+/* function getWordCount(sentence) {
+  let sum = 1;
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] === ' ') {
+      sum++;
+    }
+  }
+  return sum;
+}
