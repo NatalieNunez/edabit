9ea4e890c6cc8ecb5577a00b3d411602ea0b952b
NatalieNunez <natalie.nunez17@yahoo.com>
2021-01-18 16:52:36 -0800

    Completed Check Title

--- checktitle.js
+++ checktitle.js
@@ -1,6 +1,17 @@
 function checkTitle(title) {
   let splitArr = title.join(" ");
-  for (let i = 0; i < splitArr.length; i++) {}
+  let arr = [];
+  for (let i = 0; i < splitArr.length; i++) {
+    arr.push(splitArr[i][0]);
+  }
+  let str = arr.join("");
+  let result = true;
+  for (let j = 0; j < str.length; j++) {
+    if (str[j] !== str[j].toUpperCase()) {
+      result = false;
+    }
+  }
+  return result;
 }
 /*input string
 output boolean
