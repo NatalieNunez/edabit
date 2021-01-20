1f419f37c612f182fc37a388f4c139f7abdcd7ed
NatalieNunez <natalie.nunez17@yahoo.com>
2021-01-19 22:10:20 -0800

    Attempt at RPS, psuedocode

---
+++ rockpaperscissors.js
@@ -0,0 +1,16 @@
+function calculateScore(games) {
+	let abScore = 0;
+	let benScore = 0;
+	let val = 
+	for (let i = 0; i < games.length; i++) {
+		for (let j = 0; j < i.length; j++) {
+			if ()
+		}
+	}
+}
+	/* input : an array of arrays containing strings  output : string (return result)
+	for loop to acces first array, second for loop to access each array inside
+	containers for abigails and bensons scores; compare their results (index 0 and 1)
+	update the value of their score depending on who wins, if strings are equal, increment both
+	after looping through each array and updating scores, compare the score variables and return the name of the winner
+	R > S; R < P; S > P
