3d101b4438dd9e09b442071a0c93f02d0b7580b8
NatalieNunez <natalie.nunez17@yahoo.com>
2021-01-20 23:24:56 -0800

    added instructions and tests

--- alphasoup.js
+++ alphasoup.js
@@ -1,3 +1,10 @@
 function AlphabetSoup(str) {
   return str.split("").sort().join("");
 }
+/*
+AlphabetSoup("hello") ➞ "ehllo"
+AlphabetSoup("edabit") ➞ "abdeit"
+
+instructions: 
+Create a function that takes a string and returns a string with its letters in alphabetical order
+ */
