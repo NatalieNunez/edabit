2ab339322cbcab983bda7ba4943d660a0be10489
NatalieNunez <natalie.nunez17@yahoo.com>
2021-01-22 23:16:59 -0800

    Updated problem

--- reversecase.js
+++ reversecase.js
@@ -1,13 +1,11 @@
 function reverseCase(str) {
-  let arr = str.split();
-  let newArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === arr[i].toUpperCase) {
-      newArr.push(arr[i].toLowerCase);
+  let revStr = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i].toUpperCase) {
+      str[i].toLowerCase;
     }
     if (str[i] === str[i].toLowerCase) {
-      newArr.push(arr[i].toUpperCase);
+      str[i].toUpperCase;
     }
   }
-  return newArr.join("");
 }
