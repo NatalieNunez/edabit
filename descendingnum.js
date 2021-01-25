07c7f0970ecacca7e9a034c403a22ca4aa84f9ff
NatalieNunez <natalie.nunez17@yahoo.com>
2021-01-24 22:12:50 -0800

    Added instructions and tests

--- descendingnum.js
+++ descendingnum.js
@@ -4,3 +4,16 @@ function sortDescending(num) {
   let str = numArr.join("");
   return parseInt(str, 10);
 }
+
+/* function sortDecending(num) {
+	return parseInt(num.toString().split('').sort((a,b) => b - a).join(''))
+}
+
+instructions:
+Create a function that takes any nonnegative number as an argument and return 
+it with it's digits in descending order. Descending order is when you sort from highest to lowest.
+
+sortDescending(123) ➞ 321
+sortDescending(1254859723) ➞ 9875543221
+sortDescending(73065) ➞ 76530
+*/
