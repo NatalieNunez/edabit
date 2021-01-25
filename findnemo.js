721f9af7f421ce4682277d41253c37640fcd3728
NatalieNunez <natalie.nunez17@yahoo.com>
2021-01-25 11:43:00 -0800

    Added instructions an examples for reference

--- findnemo.js
+++ findnemo.js
@@ -7,3 +7,12 @@ function findNemo(sentence) {
     return "I can't find Nemo :(";
   }
 }
+
+// ##### Instructions #####
+// You're given a string of words. You need to find the word "Nemo", and return a string like this:
+// "I found Nemo at [the order of the word you find nemo]!".
+// If you can't find Nemo, return "I can't find Nemo :("
+
+// ##### Examples #####
+// findNemo("I am finding Nemo !") ➞ "I found Nemo at 4!"
+// findNemo("Nemo is me") ➞ "I found Nemo at 1!"
