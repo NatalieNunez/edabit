d97b31337eb69f5d17ddb20a42ba5b2b3eaa0998
NatalieNunez <natalie.nunez17@yahoo.com>
2021-01-29 00:14:29 -0800

    attempted hard problem

--- correcttitle.js
+++ correcttitle.js
@@ -1,19 +1,19 @@
 function correctTitle(str) {
-  let arr = str.split(" "); // i need to split at spaces AND hyphens but dont know how to do both
-  let newArr = []; //
+  let arr = str.split(" ");
+  let newArr = [];
   let indexArr = [];
   let store = [];
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === "and" || "the" || "of" || "in") {
       newArr.push(arr[i].toLowerCase());
     } else {
-      let indexArr = arr[i].split(""); // created this array so i can make the strings in arr become their own arrays
+      let indexArr = arr[i].split("");
     }
     for (let j = 0; j < indexArr.length; j++) {
-      if (indexArr[j] < 1) {
+      if (indexArr[j] === indexArr[0]) {
         store.push(indexArr[j].toUpperCase());
       }
-      if (indexArr[j] >= 1) {
+      if (indexArr[j] !== indexArr[0]) {
         store.push(indexArr[j].toLowerCase());
       }
       if (indexArr[j] === "-") {
