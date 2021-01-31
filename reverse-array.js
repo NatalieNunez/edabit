42d73382608e463aaa4c46d57961abde08957862
NatalieNunez <natalie.nunez17@yahoo.com>
2021-01-30 19:13:19 -0800

    Refactored reverse array problem

--- reverse-array.js
+++ reverse-array.js
@@ -1,7 +1,7 @@
 function reverse(arr) {
-	let storeArr = [];
-	for (i = arr.length-1; i >=0; i--) {
-		storeArr.push(arr[i]);
-	}
-	return storeArr
+  let storeArr = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    storeArr.push(arr[i]);
+  }
+  return storeArr;
 }
