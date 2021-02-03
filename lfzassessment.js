bbfcbd3cb40dc4b56a61f8759e611372c9fc4327
NatalieNunez <natalie.nunez17@yahoo.com>
2021-02-03 15:53:04 -0800

    avnum property stored as a property

--- lfzassessment.js
+++ lfzassessment.js
@@ -1,6 +1,6 @@
 let counter = 10; // step 1
 let numbers = []; // step 2
-for (let i = 0; i <= 10; i++) {
+for (let i = 0; i < 10; i++) {
   numbers.push(counter+=73);
 } // step 3
 console.log(numbers); // step 4
@@ -13,18 +13,20 @@ let stats = {}; // step 6
 
 function getStats(statsObj, numbersArr){ // step 7ab
     let arrTotal = 0;
-let arrAverage = 0;
-  let largNum = 0;
+    let arrAverage = 0;
+    let largNum = 0;
   for (let i = 0; i < numbersArr.length; i++) {
-    if (numbersArr[i] > largNum) {
+    if (numbersArr[i] >= largNum) {
       largNum = numbersArr[i];
     }
+    statsObj.largestNum = largNum;
   }
   for (let i = 0; i < numbersArr.lenght; i++) {
     arrTotal = arrTotal + numbersArr[i];
-  }
-  arrAverage = arrTotal/numbersArr.length;
-  statsObj.largestNum = largNum;
-  statsObj.averageNum = arrAverage; 
+    arrAverage = (arrTotal/numbersArr.length);
+    console.log(arrAverage);
+  } 
+  statsObj.averageNum = arrAverage;
   return statsObj;
 }
+console.log(getStats(stats, numbers));
