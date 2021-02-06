f93a594a810ba9ea9df8a7824124725045e90729
NatalieNunez <natalie.nunez17@yahoo.com>
2021-02-05 21:41:21 -0800

    Refactored Calculate fuel

--- calculatefuel.js
+++ calculatefuel.js
@@ -6,7 +6,11 @@ function calculateFuel(n) {
 	}
 }
 
-/* 
+/* function calculateFuel(n) {
+    return n*10 >= 100 ? n*10 : 100;
+}
+
+
 ##### INSTRUCTIONS #####
 A vehicle needs 10 times the amount of fuel than the distance it travels. However, it must always carry a minimum of 100 fuel before setting off.
 Create a function which calculates the amount of fuel it needs, given the distance.
