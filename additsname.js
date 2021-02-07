3de70e83ab86c76b1de92c3898157fb4caea73e5
NatalieNunez <natalie.nunez17@yahoo.com>
2021-02-06 19:50:40 -0800

    Added instructions and examples

--- additsname.js
+++ additsname.js
@@ -2,3 +2,15 @@ function addName(obj, name, value) {
 	obj[name] = value;
 	return obj;
 }
+
+/*
+##### INSTRUCTIONS #####
+Given three arguments ⁠— an object obj of the stolen items, the pets name and a 
+value ⁠— return an object with that name and value in it (as key-value pairs).
+
+##### EXAMPLES #####
+addName({}, "Brutus", 300) ➞ { Brutus: 300 }
+addName({ piano: 500 }, "Brutus", 400) ➞ { piano: 500, Brutus: 400 }
+addName({ piano: 500, stereo: 300 }, "Caligula", 440) ➞ { piano: 500, stereo: 300, Caligula: 440 }
+
+*/
