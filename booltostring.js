3274ae1821636a2c3f0aebcb5c2274f6c049badb
NatalieNunez <natalie.nunez17@yahoo.com>
2021-02-09 00:08:09 -0800

    completed easy edabit boolean to string

---
+++ booltostring.js
@@ -0,0 +1,17 @@
+function boolToString(flag) {
+	if (flag === true) {
+		return "true";
+	} else {
+		return "false";
+	}
+}
+/*
+
+##### INSTRUCTIONS #####
+Create a function that takes a boolean variable flag and returns it as a string.
+
+##### TESTS #####
+boolToString(true) ➞ "true"
+boolToString(false) ➞ "false"
+
+*/
