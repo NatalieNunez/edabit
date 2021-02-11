d3b105b20753cdcda677bf8d38389af77ed1f9da
NatalieNunez <natalie.nunez17@yahoo.com>
2021-02-10 16:48:23 -0800

    added instructions

--- returnnegative.js
+++ returnnegative.js
@@ -8,3 +8,10 @@ function returnNegative(n) {
 		return n;
 	}
 }
+
+/*
+##### INSTRUCTIONS #####
+Create a function that takes a number as an argument and returns 
+negative of that number. Return negative numbers without any change.
+Zero should return 0
+*/
